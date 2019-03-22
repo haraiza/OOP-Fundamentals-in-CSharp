3e07fb74200f5ad14f09020246ddd18332ed48d1
haraiza <humbertoaraiza@gmail.com>
2019-03-21 17:30:00 -0700

    3_21_2019

--- ACM/ACM.BL/Customer.cs
+++ ACM/ACM.BL/Customer.cs
@@ -8,6 +8,24 @@ namespace ACM.BL
 {
     public class Customer
     {
+
+        public Customer() : this(0)
+        {
+
+        }
+
+        public Customer(int customerId)
+        {
+            CustomerId = customerId;
+            AddressList = new List<Address>();
+        }
+
+        #region properties
+
+        public int CustomerType { get; set; }
+
+        public List<Address> AddressList { get; set; }
+
         public int CustomerId { get; private set; }
 
         public string EmailAddress { get; set; }
@@ -46,5 +64,23 @@ namespace ACM.BL
         }
 
         public static int InstanceCount { get; set; }
+        #endregion
+
+        #region methods
+        /// <summary>
+        /// Validates the customer data
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+            return isValid;
+        }
+
+        #endregion
+
     }
 }
---
+++ ACM/ACM.BLTests/CustomerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL.Tests
+{
+    [TestClass()]
+    public class CustomerTests
+    {
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
